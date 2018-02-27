2edb2aca6b734c003cc4ac1726b6ec630e592cc9
crkacer <minhducng93@gmail.com>
2018-02-27 00:25:32 -0500

    Add GET Request for Main page Client-side gets data in javascript

---
+++ Cryptfolio/Views/main.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Master.Master" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="Cryptfolio.Views.WebForm4" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Style" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Body" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Script" runat="server">
+    <script type="text/javascript">
+        var all_coins = <%=ALL_COINS%>;
+        console.log(all_coins);
+    </script>
+</asp:Content>
---
+++ Cryptfolio/Views/main.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cryptfolio.Views
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+
+        protected String Send_GET_REQUEST(String[] coins, String[] currencies)
+        {
+            // generate coins string
+            String listCoins = "";
+            for (int i = 0; i < coins.Length - 1; i++)
+            {
+                listCoins += coins[i] + ",";
+            }
+            listCoins += coins[coins.Length - 1];
+            // generate currencies string
+            String listCurrencies = "";
+            for (int i = 0; i < currencies.Length - 1; i++)
+            {
+                listCurrencies += currencies[i] + ",";
+            }
+            listCurrencies += currencies[currencies.Length - 1];
+
+            // handle GET request
+            StringBuilder sb = new StringBuilder();
+
+            byte[] buf = new byte[8192];
+
+            //do get request
+
+            String url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + listCoins + "&tsyms=" + listCurrencies;
+            HttpWebRequest request = (HttpWebRequest)
+                WebRequest.Create(url);
+
+
+            HttpWebResponse response = (HttpWebResponse)
+                request.GetResponse();
+
+
+            Stream resStream = response.GetResponseStream();
+
+            string tempString = null;
+            int count = 0;
+            //read the data and print it
+            do
+            {
+                count = resStream.Read(buf, 0, buf.Length);
+                if (count != 0)
+                {
+                    tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                    sb.Append(tempString);
+                }
+            }
+            while (count > 0);
+            return sb.ToString();
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String a = Send_GET_REQUEST(new String[20] { "BTC", "ETH", "XRP", "BCH", "NEO", "LTC", "ADA", "EOS", "XLM", "VEN", "IOTA", "XMR", "TRX", "ETC", "LSK", "QTUM", "OMG", "XVG", "USDT", "XRB" }, new String[1] { "USD" });
+            // Response.Write(a);
+
+
+        }
+
+        // Handle BUY coin
+
+        protected void Buy_Coin(String coin, int userID)
+        {
+            String price = Send_GET_REQUEST(new String[1] { coin }, new String[1] { "USD" });
+
+        }
+
+        protected string ALL_COINS
+        {
+            get
+            {
+                return Send_GET_REQUEST(new String[20] { "BTC", "ETH", "XRP", "BCH", "NEO", "LTC", "ADA", "EOS", "XLM", "VEN", "IOTA", "XMR", "TRX", "ETC", "LSK", "QTUM", "OMG", "XVG", "USDT", "XRB" }, new String[1] { "USD" });
+
+            }
+        }
+
+        protected string SINGLE_COIN(String coin)
+        {
+            return Send_GET_REQUEST(new String[1] { coin }, new String[1] { "USD" });
+        }
+
+        // Handle SELL coin
+
+        protected void Sell_Coin(String coin, int userID)
+        {
+            String price = Send_GET_REQUEST(new String[1] { coin }, new String[1] { "USD" });
+        }
+    }
+}
